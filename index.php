f87c6d206846c3d5eca4370fadbf6c90c7965109
Minh Kiet <kietminh070502@gmail.com>
2023-08-14 19:14:33 +0700

    update data

--- index.php
+++ index.php
@@ -11,36 +11,5 @@ try {
     echo "Connection Error " . $e->getMessage();
 }
 
-$statement = $pdo->prepare("SELECT * FROM students WHERE firstname = ?");
-$statement->execute(['Firstname1']);
-$result = $statement->fetchAll(PDO::FETCH_ASSOC);
-echo '<pre>';
-print_r($result);
-echo '</pre>';
-
-
-
-$statement = $pdo->prepare("SELECT * FROM students WHERE lastname LIKE ?");
-$statement->execute(['%name%']);
-$result = $statement->fetchAll(PDO::FETCH_ASSOC);
-echo '<pre>';
-print_r($result);
-echo '</pre>';
-
-
-
-$statement = $pdo->prepare("SELECT * FROM students WHERE lastname IN (?,?)");
-$statement->execute(['Lastname1','Kiet']);
-$result = $statement->fetchAll(PDO::FETCH_ASSOC);
-echo '<pre>';
-print_r($result);
-echo '</pre>';
-
-
-
-// $statement = $pdo->prepare("SELECT firstname, email, city FROM students WHERE city IN ( SELECT institute_city FROM institutes)");
-// $statement->execute();
-// $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-// echo '<pre>';
-// print_r($result);
-// echo '</pre>';
+$statement = $pdo->prepare("UPDATE students SET email = ? WHERE firstname = ?");
+$statement->execute(['haha@gmail.com','Firstname1']);
