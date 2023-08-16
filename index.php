b679e656d72078c50eb7fe4d6de735e107f04008
Minh Kiet <kietminh070502@gmail.com>
2023-08-16 18:51:27 +0700

    union clause

--- index.php
+++ index.php
@@ -12,90 +12,17 @@ try {
 }
 
 
-$statement = $pdo->prepare("SELECT s.firstname, c.city_name
-                            FROM students s
-                            INNER JOIN cities c
-                            ON s.city_id = c.city_id
-                            WHERE c.city_name = ?");
-$statement->execute(['HCM']);
-$result = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-
-//LEFT JOIN
-// $statement = $conn->prepare("SELECT
-//                             s.id as sid,
-//                             s.department_id,
-//                             s.firstname,
-//                             s.lastname,
-//                             s.email,
-//                             s.age,
-//                             s.city,
-//                             d.id as did,
-//                             d.name as dname
-//                             FROM students s
-//                             LEFT JOIN departments d
-//                             ON s.department_id = d.id");
-
-// $statement->execute();
-// $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-
-
-//RIGHT JOIN
-// $statement = $conn->prepare("SELECT
-//                             s.id as sid,
-//                             s.department_id,
-//                             s.firstname,
-//                             s.lastname,
-//                             s.email,
-//                             s.age,
-//                             s.city,
-//                             d.id as did,
-//                             d.name as dname
-//                             FROM students s
-//                             RIGHT JOIN departments d
-//                             ON s.department_id = d.id");
-
-// $statement->execute();
-// $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-
-
-//FULL JOIN
-// $statement = $conn->prepare("SELECT
-//                             s.id as sid,
-//                             s.department_id,
-//                             s.firstname,
-//                             s.lastname,
-//                             s.email,
-//                             s.age,
-//                             s.city,
-//                             d.id as did,
-//                             d.name as dname
-//                             FROM students s
-//                             LEFT JOIN departments d
-//                             ON s.department_id = d.id
+$statement = $pdo->prepare("SELECT firstname, lastname
+                            FROM students
+                            WHERE lastname = ?
                             
-//                             UNION
-
-//                             SELECT
-//                             s.id as sid,
-//                             s.department_id,
-//                             s.firstname,
-//                             s.lastname,
-//                             s.email,
-//                             s.age,
-//                             s.city,
-//                             d.id as did,
-//                             d.name as dname
-//                             FROM students s
-//                             RIGHT JOIN departments d
-//                             ON s.department_id = d.id
-//                             ");
-
-// $statement->execute();
-// $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-
+                            UNION
+                            
+                            SELECT firstname, lastname
+                            FROM students
+                            WHERE lastname = ?");
+$statement->execute(['Kiet','Phuc']);
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 
 echo '<pre>';
