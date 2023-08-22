8349d7fcddb7f540b96cf9e4714ca6719d9f0dc1
Minh Kiet <kietminh070502@gmail.com>
2023-08-22 18:51:45 +0700

    Registration

---
+++ footer.php
@@ -0,0 +1,9 @@
+</div>
+        </div>
+        <div class="copyright mt_10">
+            Copyright &copy; Lai Minh Kiet. All Rights Reserved.
+        </div>
+    </div>
+    
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,113 +1,42 @@
 <?php
-$servername = "localhost";
-$dbname = "php_practice";
-$username = "root";
-$password = "";
-
-try {
-    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Error Connection " . $e->getMessage();
-}
+include_once('header.php');
 ?>
-<!DOCTYPE html>
-<html lang="en">
+<h2>Welcome to our website</h2>
+<p>
+    You can register in this website and create account.
+</p>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        a.links {
-            width: 40px;
-            height: 40px;
-            line-height: 40px;
-            text-align: center;
-            display: inline-block;
-            text-decoration: none;
-            background: lightblue;
-            color: black;
-            font-size: 20px;
-            margin-right: 5px;
-            margin-bottom: 20px;
-        }
-    </style>
-</head>
-
-<body>
-    <div style="width: 400px; margin: 0 auto;">
-        <h3>Student List</h3>
-
-        <?php
-        $per_page = 2;
-        $statement = $pdo->prepare("SELECT * FROM students");
-        $statement->execute();
-        $total = $statement->rowCount();
-        $total_pages =  ceil($total / $per_page);
-
-        if (!isset($_REQUEST['p'])) {
-            $start = 1;
-        } else {
-            $start = $per_page * ($_REQUEST['p'] - 1) + 1;
-        }
-
-        $j = 0;
-        $k = 0;
-        $arr1 = [];
-        $result =  $statement->fetchAll();
-        foreach ($result as $row) {
-            $j++;
-            if ($j >= $start) {
-                $k++;
-                if ($k > $per_page) {
-                    break;
-                }
-                // echo $row['id']. ' ';
-                $arr1[] = $row['id'];
-            }
-        }
-
-        ?>
-        <?php
-        $statement = $pdo->prepare("SELECT * FROM students");
-        $statement->execute();
-        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-        foreach ($result as $row) {
-            if (!in_array($row['id'], $arr1)) {
-                continue;
-            }
-            echo "<div style = 'padding: 10px; margin-bottom: 10px; background: #ddd;'>";
-            echo "Name: " . $row['firstname'] . ' ' . $row['lastname'] . '<br>';
-            echo "Email: " . $row['email'];
-            echo "</div>";
-        }
-
-
-        if (isset($_REQUEST['p'])) {
-            if ($_REQUEST['p'] == 1) {
-                echo '<a class="links" href="javascript:void;" style="background:#ddd;"> << </a>';
-            } else {
-                echo '<a class="links" href="http://localhost/php_practice/index.php?p=' . ($_REQUEST['p'] - 1) . '"> << </a>';
-            }
-        } else {
-            echo '<a class="links" href="javascript:void;" style="background:#ddd;"> << </a>';
-        }
-
-        for ($i = 1; $i <= $total_pages; $i++) {
-            echo '<a class="links" href="http://localhost/php_practice/index.php?p=' . $i . '">' . $i . '</a>';
-        }
-        if (isset($_REQUEST['p'])) {
-            if ($_REQUEST['p'] == $total_pages) {
-                echo '<a class="links" href="javascript:void;" style="background:#ddd;"> >> </a>';
-            } else {
-                echo '<a class="links" href="http://localhost/php_practice/index.php?p=' . ($_REQUEST['p'] + 1) . '"> >> </a>';
-            }
-        } else {
-            echo '<a class="links" href="http://localhost/php_practice/index.php?p=2"> >> </a>';
-        }
-        ?>
-    </div>
-</body>
-
-</html>
+<h2 class="mt_20 mb_10">All Registered Users</h2>
+<table class="t1">
+    <tr>
+        <th>SL</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Email</th>
+        <th>Phone</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Smith</td>
+        <td>Johnson</td>
+        <td>smith@gmail.com</td>
+        <td>111111</td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>Ray</td>
+        <td>Manner</td>
+        <td>ray@gmail.com</td>
+        <td>222222</td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>Jessica</td>
+        <td>Robert</td>
+        <td>jessica@gmail.com</td>
+        <td>3333333</td>
+    </tr>
+</table>
+<?php
+include_once('footer.php');
+?>
